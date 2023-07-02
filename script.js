4ec1ce11dcaa846537e08beb1863c5bc6fe9e43c
Ezequiel Bernard <ezequielbernard2@gmail.com>
2023-07-02 13:59:27 -0300

    js0

--- script.js
+++ script.js
@@ -10,11 +10,10 @@ function myFunction() {
 (function () {
   "use strict";
 
-  // define variables
+
   var items = document.querySelectorAll(".receta-shangai li");
 
-  // check if an element is in viewport
-  // http://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport
+
   function isElementInViewport(el) {
     var rect = el.getBoundingClientRect();
     return (
@@ -34,7 +33,7 @@ function myFunction() {
     }
   }
 
-  // listen for events
+
   window.addEventListener("load", callbackFunc);
   window.addEventListener("resize", callbackFunc);
   window.addEventListener("scroll", callbackFunc);
